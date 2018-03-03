acdfbdc045b13f82b31347e7bd18a0b8103e5f26
foudilredaoui <df_redaoui@esi.dz>
2018-03-03 23:23:54 +0100

    YelpCamp initial Routes

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var app = express();
+app.set("view engine","ejs");
+app.get("/",function(req,res){
+    res.render("landing");
+});
+app.get("campgrounds",function(req,res){
+    var campgrounds = [
+        {name:"Salman Creek",image:""},
+        {name:"Granite Hill",image:""},
+        {name:"Mountain Goat's Rest",image:""}
+    ]
+
+res.render("campgrounds");
+});
+
+app.listen(3000,function(){
+ console.log(" the YelpCamp serverS has started");
+});
