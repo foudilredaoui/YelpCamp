c0dbd325a244fe7311edb925faa7d73b445f0981
foudilredaoui <df_redaoui@esi.dz>
2018-03-04 10:56:11 +0100

    YelpCamp Layout

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ app.set("view engine","ejs");
 app.get("/",function(req,res){
     res.render("landing");
 });
-app.get("campgrounds",function(req,res){
+app.get("/campgrounds",function(req,res){
     var campgrounds = [
         {name:"Salman Creek",image:""},
         {name:"Granite Hill",image:""},
         {name:"Mountain Goat's Rest",image:""}
     ]
 
-res.render("campgrounds");
+res.render("campgrounds",{campgrounds:campgrounds});
 });
 
 app.listen(3000,function(){
