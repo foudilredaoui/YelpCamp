65f24a86d47c78adcbcdbac927ee4a66a2b070b5
foudilredaoui <df_redaoui@esi.dz>
2018-03-10 23:03:02 +0100

    get data from DB

--- app.js
+++ app.js
@@ -2,24 +2,41 @@ var express = require("express"),
     app = express(),
     bodyparser = require("body-parser"),
     mongoose = require("mongoose")
-var campgrounds = [{
-    name: "Salman Creek",
-    image: "https://pixabay.com/get/ec31b90f2af61c2ad65a5854ee4d459fe270e7c818b4134194f6c379a0ea_340.jpg"
-},
-{
-    name: "Granite Hill",
-    image: "https://pixabay.com/get/e136b80728f31c2ad65a5854ee4d459fe270e7c818b4134194f6c379a0ea_340.jpg"
-},
-{
-    name: "Mountain Goat's Rest",
-    image: "https://pixabay.com/get/ea34b50f2cf4013ed95c4518b7444795ea76e5d004b0144395f1c77ba7eab2_340.jpg"
-}
-];
+// var campgrounds = [{
+//     name: "Salman Creek",
+//     image: "https://pixabay.com/get/ec31b90f2af61c2ad65a5854ee4d459fe270e7c818b4134194f6c379a0ea_340.jpg"
+// },
+// {
+//     name: "Granite Hill",
+//     image: "https://pixabay.com/get/e136b80728f31c2ad65a5854ee4d459fe270e7c818b4134194f6c379a0ea_340.jpg"
+// },
+// {
+//     name: "Mountain Goat's Rest",
+//     image: "https://pixabay.com/get/ea34b50f2cf4013ed95c4518b7444795ea76e5d004b0144395f1c77ba7eab2_340.jpg"
+// }
+// ];
 
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyparser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 // SCHEMA SEtUP
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var campground = mongoose.model("Campground",campgroundSchema);
+// campground.create({
+//     name: "Granite Hill",
+//     image: "https://pixabay.com/get/e136b80728f31c2ad65a5854ee4d459fe270e7c818b4134194f6c379a0ea_340.jpg"
+// },function(err,campground){
+//   if(err){
+//       console.log(err);
+//   }else{
+//       console.log("newly created campground :");
+//       console.log(campground);
+//   }
+// });
 
 
 
@@ -28,10 +45,17 @@ app.get("/", function (req, res) {
     res.render("landing");
 });
 app.get("/campgrounds", function (req, res) {
-  
-    res.render("campgrounds", {
-        campgrounds: campgrounds
+   // get all campgrounds from db
+    campground.find({},function(err,campgrounds){
+        if(err){
+            Console.log(err);
+        }else{
+            res.render("campgrounds", {
+                campgrounds: campgrounds
+            });
+        }
     });
+    
 });
 
 app.post("/campgrounds", function (req, res) {
