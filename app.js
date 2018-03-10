9c10924601fbf3c534cd3695fa94bfd65182eeae
foudilredaoui <df_redaoui@esi.dz>
2018-03-10 23:56:03 +0100

    edit the new form

--- app.js
+++ app.js
@@ -66,9 +66,11 @@ app.post("/campgrounds", function (req, res) {
     // get data from form and add to the campgrounds array
     var name = req.body.name;
     var image = req.body.image;
+    var desc = req.body.description;
     var newCampground = {
         name: name,
-        image: image
+        image: image,
+        description:desc
     };
     // create a new campground and save to the DB
     campground.create(newCampground, function (err, newlycampground) {
