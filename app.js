d494aa1aab39589190292bb80bc96ec8144a26d1
foudilredaoui <df_redaoui@esi.dz>
2018-05-01 15:43:53 +0100

    creating comments

--- app.js
+++ app.js
@@ -80,12 +80,34 @@ app.get("/campgrounds/:id/comments/new",function(req,res){
         if (err) {
             console.log(err);
         }else{
-            // res.render("comments/new",{campground: campground});
-            console.log(campground);
+            res.render("comments/new",{campground: campground});
+         
         }
     })
 });
 
+app.post("/campgrounds/:id/comments",function(req,res){
+    // lookup campground using ID
+    campground.findById(req.params.id,function(err,campground){
+        if (err) {
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+            Comment.create(req.body.comment,function(err,comment){
+                if (err) {
+                    console.log(err);
+                } else {
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect('/campgrounds/'+campground._id);
+                }
+            });
+        }
+    })
+    // create new comment
+    //connect new comment to campground
+    //redirect campground show page
+});
 app.listen(3000, function () {
     console.log(" the YelpCamp serverS has started");
 });
