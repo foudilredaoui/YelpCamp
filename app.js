acc7702e7cbc4f9dcd52ea241b2b7ed213799bf4
foudilredaoui <df_redaoui@esi.dz>
2018-05-01 17:18:48 +0100

    styling Comments 2

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(bodyparser.urlencoded({
     extended: true
 }));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 seedDB();
 
 app.get("/", function (req, res) {
